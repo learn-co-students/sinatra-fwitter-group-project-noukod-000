9dfcdf5d806829d4bddae68c131f007ac7b2368f
mxdavis <md01@me.com>
2017-04-24 03:32:08 +0300

    enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,16 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+		set :session_secret, "password_security"
   end
 
   get '/signup' do
+    erb :'users/create_user'
+  end
+
+  post '/signup' do
+    redirect to '/'
   end
 
 end
