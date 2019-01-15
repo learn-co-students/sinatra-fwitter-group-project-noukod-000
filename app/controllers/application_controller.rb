88860a3d2349c96ed8cb547c3104de5d9f94c265
Samuel Cornet <samuel@noukod.com>
2019-01-15 19:30:29 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,29 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+
+  enable :sessions
+  set :session_secret, "my_application_secret"
+  set :views, Proc.new { File.join(root, "../views/") }
+
+  get '/' do
+    erb :index
+  end
+end
+
+
+class ApplicationController < Sinatra::Base
 
   configure do
-    set :public_folder, 'public'
+    # set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "fwitter_aka_flatiron_twitter"
+  end
+
+  get '/' do
+    erb :index
   end
 
 end
