676f699e43cba8bddd8ef8ea03944761581d2ec3
Certil Remy <certil@noukod.com>
2019-01-17 18:39:05 +0000

    Done.

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  validates :username, presence: true
+  validates :email, presence: true
+  has_secure_password
+  has_many :tweets
+
+  def self.find_by_slug(slug)
+    self.all.find{ |user| user.slug == slug }
+  end
+
+  def slug
+    self.username.gsub(" ", "-").downcase
+  end
+end
