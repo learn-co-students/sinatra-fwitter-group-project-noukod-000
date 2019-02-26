b71e9b4637405af04d02c95103946470dffa9cc4
Merline Belizaire <merline@noukod.com>
2019-02-26 21:39:18 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   get "/signup" do
+    if !logged_in?
       erb :"users/create_user"
+    else
+      redirect to '/tweets'
     end
 
     post '/signup' do
@@ -12,6 +15,7 @@ class UsersController < ApplicationController
     # end
     user = User.create(:username => params[:username], :email => params[:email])
         user.authenticate(params[:password])
+        user.save
       session[:user_id] = user.id
      redirect "/tweets"
 
@@ -30,8 +34,12 @@ class UsersController < ApplicationController
   end
 
   get "/login" do
+    if !logged_in?
     erb :'/users/login'
+  else
+    redirect to '/tweets'
   end
+end
 
   post '/login' do
     user = User.find_by(:username => params[:username])
