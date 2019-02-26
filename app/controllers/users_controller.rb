07dd8e1eaa964c31a211983ba2cc092adccf5b8e
Merline Belizaire <merline@noukod.com>
2019-02-26 20:27:51 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,4 +23,23 @@ class UsersController < ApplicationController
     erb :'/users/show'
   end
 
+  get "/login" do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+       redirect "/tweets"
+     else
+       redirect "/login"
+     end
+ end
+
+ get "/logout" do
+   session.clear
+   redirect "/login"
+ end
 end
