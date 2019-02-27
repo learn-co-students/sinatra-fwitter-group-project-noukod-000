22c69d4854b130710dfa45b966f7ee58cffd7140
Merline Belizaire <merline@noukod.com>
2019-02-27 14:26:04 +0000

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,21 +25,30 @@ class UsersController < ApplicationController
   end
 
   get '/users/:slug' do
+    user = User.find_by_slug(session[:slug])
     if logged_in?
 			erb :'/users/show'
 		else
 			redirect "/login"
 		end
 
-     @user = User.find(session[:user_id])
-    erb :'/users/show'
-  end
+  #    @user = User.find(session[:user_id])
+  #   erb :'/users/show'
+   end
 
   get "/login" do
     if !logged_in?
     erb :'/users/login'
   else
     redirect to '/tweets'
+  end
+
+  user = User.find_by(:username => params[:username])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/tweets"
+  else
+    redirect "/signup"
   end
 end
 
@@ -56,7 +65,11 @@ end
  end
 
  get "/logout" do
-   session.clear
+   if logged_in?
+   session.destroy
    redirect "/login"
- end
+ else
+   redirect '/'
+  end
+end
 end
