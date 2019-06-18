bfd06f248442512398a71dd07e86da93d7d1f451
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-18 14:10:26 +0000

    save1

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -8,4 +8,16 @@ class TweetsController < ApplicationController
   end
   end
 
+  get "/tweets/new" do
+    if Helpers.is_logged_in?(session)
+      @user = User.find_by(:id => session[:user_id])
+      erb "/tweets/new"
+    end
+  end
+
+  post "/tweets" do
+    if Helpers.is_logged_in?(session)
+      @tweet = Tweet.create(params [:content])
+    end
+  end
 end
