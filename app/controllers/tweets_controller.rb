4d1154ea3933ee396d5aa121f2e137d4806c9172
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-18 14:44:48 +0000

    save2

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,23 +1,37 @@
 class TweetsController < ApplicationController
-  get "/tweets" do
-    if !Helpers.is_logged_in?(session)
-      redirect to "/login"
-    else
-    @tweets = Tweet.all
-    erb "/tweets/tweets".to_sym
-  end
-  end
-
-  get "/tweets/new" do
-    if Helpers.is_logged_in?(session)
-      @user = User.find_by(:id => session[:user_id])
-      erb "/tweets/new"
+    get "/tweets" do
+      if !Helpers.is_logged_in?(session)
+        redirect to "/login"
+      else
+      @tweets = Tweet.all
+      erb "/tweets/tweets".to_sym
     end
-  end
-
-  post "/tweets" do
-    if Helpers.is_logged_in?(session)
-      @tweet = Tweet.create(params [:content])
     end
-  end
+
+    get "/tweets/new" do
+      if Helpers.is_logged_in?(session)
+        @user = User.find_by(:id => session[:user_id])
+        erb "/tweets/new".to_sym
+      else
+        redirect to "/login"
+      end
+    end
+
+    post "/tweets" do
+      if Helpers.is_logged_in?(session)
+          tweet = Helpers.current_user(session).tweets.build(content: params[:content])
+          if tweet.save
+              redirect to "/tweets/#{tweet.id}"
+          else
+            redirect to "/tweets/new"
+          end
+      else
+          redirect to '/login'
+      end
+    end
+
+    get "/tweets/:id" do
+
+    end
+
 end
