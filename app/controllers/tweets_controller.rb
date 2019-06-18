1846d9284172ce29c7ce3e5c7e12ea958b2803d4
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-18 16:11:27 +0000

    finish with edit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -30,8 +30,57 @@ class TweetsController < ApplicationController
       end
     end
 
-    get "/tweets/:id" do
+    get "/tweets/new" do
+      if Helpers.is_logged_in?(session)
+        @user = User.find_by(:id => session[:user_id])
+        erb "/tweets/new".to_sym
+      else
+        redirect to "/login"
+      end
+    end
 
+    post "/tweets" do
+      if Helpers.is_logged_in?(session)
+          tweet = Helpers.current_user(session).tweets.build(content: params[:content])
+          if tweet.save
+              redirect to "/tweets/#{tweet.id}"
+          else
+            redirect to "/tweets/new"
+          end
+      else
+          redirect to '/login'
+      end
+    end
+
+    get "/tweets/new" do
+      if Helpers.is_logged_in?(session)
+        @user = User.find_by(:id => session[:user_id])
+        erb "/tweets/new".to_sym
+      else
+        redirect to "/login"
+      end
+    end
+
+    post "/tweets" do
+      if Helpers.is_logged_in?(session)
+          tweet = Helpers.current_user(session).tweets.build(content: params[:content])
+          if tweet.save
+              redirect to "/tweets/#{tweet.id}"
+          else
+            redirect to "/tweets/new"
+          end
+      else
+          redirect to '/login'
+      end
+    end
+
+    get "/tweets/:id" do
+      if Helpers.is_logged_in?(session)
+        @tweet = Tweet.find_by_id(params[:id])
+        erb :'tweets/show_tweet'
+      else
+        redirect to '/login'
+      end
     end
 
 end
